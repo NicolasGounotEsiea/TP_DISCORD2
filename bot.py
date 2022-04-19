8d0c84946331a8b2708be35a265053643dc0a37f
Votre Nom <ngounot@et.esiea.fr>
2022-04-19 05:37:03 +0200

    bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+from datetime import datetime
+
+class BBot(commands.Bot):
+
+    async def on_ready(self):
+        print(f'{self.user.name} has connected to Discord!')
+        time = str(datetime.now())
+        f = open("example.log", "a")
+        f.write(time + " The bot " + self.user.name + " is logged to the server !\n")
+        f.close()
+
+    
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
