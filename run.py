389219f380476377377469eb68434135eca73abd
Votre Nom <ngounot@et.esiea.fr>
2022-04-19 13:49:37 +0200

    commit

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+
+import json
+from lib2to3.pgen2 import token
 from pickle import NONE
+from sys import prefix
+
 from bot import BBot
 
 import discord
@@ -9,30 +14,76 @@ import os
 from help import MyHelp
 import logging
 from datetime import datetime
+from argparse import ArgumentParser, Namespace
 
 
 
 
 
 
-def main():
+
     
+    
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-c", "--config", help="Config file", required=True, dest="config"
+    )
+    return parser.parse_args()
+
+def get_config(config_file: str)-> dict:
+
+    with open(config_file, "r") as f:
+
+        config = json.load(f)
+
+    return config
+
+
+
+def main(config : dict) -> bool:
+
+    TOKEN = config["TOKEN"]
+    PREFIX = config["prefix"]
+
     logging.basicConfig(filename='logs.log', filemode='w', encoding='utf-8', format='%(levelname)s: %(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
     logging.info('Started')
- 
+    
     logging.info('Finished')
     intents = discord.Intents.default()
     intents.presences = True
     intents.members = True
-    bbot = BBot(command_prefix=os.getenv("prefix"), intents=intents)
+    bbot = BBot(command_prefix=PREFIX, intents=intents)
 
 
 
     bbot.add_cog(MyHelp(bbot))
   
 
-    bbot.run(os.getenv("TOKEN"))
+    bbot.run(TOKEN)
 
 
-if __name__ == '__main__':
-    main()
+   
+
+    pass
+
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+
+    #print(args.config)
+
+    #print(os.path.isfile(args.config))
+
+    #assert os.path.isfile(args.config), "Config file not found"
+
+    config = get_config(args.config)
+
+    main(config)
+
+    pass
+
+
+
