c6be789d76cb7e9f62023282ef1116e0e289bdfe
Votre Nom <ngounot@et.esiea.fr>
2022-04-20 23:32:39 +0200

    help.py

--- help.py
+++ help.py
@@ -36,7 +36,7 @@ class MyHelp(discord.ext.commands.Cog, name='Greetings module'):
             if message.content.lower() == mes:
                 await message.channel.send(rep) 
                 f = open("logs.log", "a")
-                f.write(time + message.author.name+"a utilisé la commande "+ rep + "\n")
+                f.write(time + message.author.name+" a utilisé la commande "+ rep + "\n")
                 f.close()
         if message.content.startswith("!del"):
             number = int(message.content.split()[1])
@@ -44,7 +44,7 @@ class MyHelp(discord.ext.commands.Cog, name='Greetings module'):
             for each_message in messages:
                 await each_message.delete()
                 f = open("logs.log", "a")
-                f.write(time + message.author.name+"a utilisé la commande help" + "\n")
+                f.write(time + message.author.name+" a utilisé la commande !delete" + "\n")
                 f.close()
         if message.content.startswith("!help"):
             embed=discord.Embed(title="Commandes additionnelles", color=0x553adf)
@@ -54,6 +54,9 @@ class MyHelp(discord.ext.commands.Cog, name='Greetings module'):
             embed.add_field(name="bonjour bot", value="salut!", inline=False)
             embed.add_field(name="del X", value="supprime les X dernières lignes", inline=False)
             await message.channel.send(embed=embed)
+            f = open("logs.log", "a")
+            f.write(time + message.author.name+" a utilisé la commande !help" + "\n")
+            f.close()
 
     #bot = commands.Bot(command_prefix = "!", description = "Bot de Sandra")
     #@bot.event
@@ -62,6 +65,7 @@ class MyHelp(discord.ext.commands.Cog, name='Greetings module'):
     @commands.guild_only()
     async def serverinf(self, ctx):
         """Info server"""
+        
         name = str(ctx.guild.name)
         description = str(ctx.guild.description)
 
@@ -82,8 +86,26 @@ class MyHelp(discord.ext.commands.Cog, name='Greetings module'):
         embed.add_field(name="Server ID", value=id, inline=True)
         embed.add_field(name="Region", value=region, inline=True)
         embed.add_field(name="Member Count", value=memberCount, inline=True)
+        f = open("logs.log", "a")
+        f.write(time + ctx.author.name+" a utilisé la commande !serverinf" + "\n")
+        f.close()
 
         await ctx.send(embed=embed)
+
+    @discord.ext.commands.Cog.listener()
+    async def on_reaction_add(self,reaction, user):
+        emoji2 = '\U0001F606' #:laughing:
+        emoji4 = '\U0001F60E' #:sunglasses:
+        emoji5 = '\U0001F973' #:partying_face:
+        emoji3 = '\U0001F97A' #:pleading_face:
+        emoji1 = '\U0001F4AF' #:100:
+        if not user.bot:
+            await reaction.message.add_reaction(emoji3)
+            time = str(datetime.now())
+            f = open("logs.log", "a")
+            f.write(time + " un utilisateur a ajouté une réaction" + "\n")
+            f.close()
+
 
     
 
